b30dd655f6957e93cf50686c10d32a6ce9aaed15
krish27-git <krishna.ginka27@zohomail.in>
2026-02-04 10:27:38 +0530

    Delete departments.sql

--- departments.sql
+++
@@ -1,8 +0,0 @@
--- Create departments table
-CREATE TABLE mdm_Admin.departments (
-    dept_id SERIAL PRIMARY KEY,
-    dept_name VARCHAR(100) NOT NULL,
-    location VARCHAR(100),
-    manager_name VARCHAR(100),
-    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
